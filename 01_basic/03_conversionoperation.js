36b080b09d122163a5ab159f072a1e315ebe60e6
swastik <princerajak2006@gmail.com>
2026-02-05 12:38:02 +0000

    conversion of datatypes

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -25,5 +25,59 @@ let BooleanIsLoggedIn = Boolean(IsLoggedIn)
 let someNumber = 43
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Operations ***********************
+
+let value = 8
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2^3 (2 power 3) = 8 
+// console.log(3/2);
+// console.log(5%2);
+
+let str1 = "hello"
+let str2 = " swastik"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+

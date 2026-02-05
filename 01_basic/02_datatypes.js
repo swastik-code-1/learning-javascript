de436dc2bdfd8ecfdd1a3e49e3566096ad160adf
swastik <princerajak2006@gmail.com>
2026-02-05 09:27:40 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer verson
+
+// alert(1 + 7) // we are using nodejs, not browser
+
+console.log(1 
+    +
+     7) // code readability should be high
+
+console.log("swastik")
+
+
+let name = "swastik"
+let age = 19
+let isLoggedIn = true
+let state;
+
+
+// number => 2 to power 53
+// bigint 
+// string => "swastik"
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
